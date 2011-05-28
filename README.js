b4246a35fd86862108abadda3dc345ec490b4be1
Jed Parsons <jedp@me.com>
2011-05-27 23:01:54 -0700

    deleted!

--- README.js
+++
@@ -1,9 +0,0 @@
-reloj - Timely Logging
-======================
-
-I just started reloj.  I hope it will be a way to see what's going on with your
-software and systems in real time or the recent past.
-
-
-
-
