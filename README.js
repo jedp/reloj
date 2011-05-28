6ea5c31cf6b9b219c2bd04c902a252c4c2998baa
Jed Parsons <jedp@me.com>
2011-05-27 22:56:06 -0700

    now github may think I am a good person

---
+++ README.js
@@ -0,0 +1,9 @@
+reloj - Timely Logging
+======================
+
+I just started reloj.  I hope it will be a way to see what's going on with your
+software and systems in real time or the recent past.
+
+
+
+
